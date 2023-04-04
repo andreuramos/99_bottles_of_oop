f6fff9f5434a7878f11638efee3a242453476399
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2023-04-04 09:02:56 +0200

    skelleton of bottles script

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+require_once "vendor/autoload.php";
+
+use BottlesOfOop\BottlesOfOOP;
+
+$bottles = new BottlesOfOOP();
+$bottles->run();
+
---
+++ src/BottlesOfOOP.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace BottlesOfOop;
+
+class BottlesOfOOP
+{
+    public function run(): void
+    {
+        echo "running";
+    }
+}
