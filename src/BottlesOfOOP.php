09c5ef5f9babe249cc37072dbf18368a03ca31e9
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2023-04-04 12:14:08 +0200

    initial implementation

--- src/BottlesOfOOP.php
+++ src/BottlesOfOOP.php
@@ -6,6 +6,23 @@ class BottlesOfOOP
 {
     public function run(): void
     {
-        echo "running";
+        for ($i=99; $i>=0; $i--) {
+            echo $this->verse($i);
+        }
+    }
+
+    private function verse(int $i)
+    {
+        $initialBeersOnTheWall = $i !== 0 ? (string) $i : "No more";
+        $beersLeftOnTheWall = $i - 1 !== 0 ? (string) $i-1 : "no more";
+
+        $verseAction = "Take one down and pass it around";
+        if ($i === 0) {
+            $verseAction = "Go to the store and buy some more";
+            $beersLeftOnTheWall = "99";
+        }
+
+        return "$initialBeersOnTheWall bottles of beer on the wall, " . strtolower($initialBeersOnTheWall) . " bottles of beer.\n" .
+            "{$verseAction}, $beersLeftOnTheWall bottles of beer in the wall.\n\n";
     }
 }
