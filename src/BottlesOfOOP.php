e227c6caf58aa0708f1fd1f2f3160d5d68ac4954
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2023-04-17 14:52:11 +0200

    now test pass, but code is uglier :/

--- src/BottlesOfOOP.php
+++ src/BottlesOfOOP.php
@@ -15,16 +15,23 @@ class BottlesOfOOP
 
     private function verse(int $i)
     {
-        $initialBeersOnTheWall = $i !== 0 ? (string) $i : "No more";
-        $beersLeftOnTheWall = $i - 1 !== 0 ? (string) $i-1 : "no more";
+        $initialBeersOnTheWall = $i !== 0 ? (string)$i : "No more";
+        $beersLeftOnTheWall = $i - 1 !== 0 ? (string)$i - 1 : "no more";
+        $pluralizedInitialBottles  = $pluralizedLeftBottles = "bottles";
 
         $verseAction = "Take one down and pass it around";
-        if ($i === 0) {
+        if ($i === 2) {
+            $pluralizedLeftBottles = "bottle";
+        } else if ($i === 1) {
+            $verseAction = "Take it down and pass it around";
+            $pluralizedInitialBottles = "bottle";
+        } else if ($i === 0) {
             $verseAction = "Go to the store and buy some more";
             $beersLeftOnTheWall = "99";
         }
 
-        return "$initialBeersOnTheWall bottles of beer on the wall, " . strtolower($initialBeersOnTheWall) . " bottles of beer.\n" .
-            "{$verseAction}, $beersLeftOnTheWall bottles of beer on the wall.\n\n";
+
+        return "$initialBeersOnTheWall $pluralizedInitialBottles of beer on the wall, " . strtolower($initialBeersOnTheWall) . " $pluralizedInitialBottles of beer.\n" .
+            "{$verseAction}, $beersLeftOnTheWall $pluralizedLeftBottles of beer on the wall.\n\n";
     }
 }
